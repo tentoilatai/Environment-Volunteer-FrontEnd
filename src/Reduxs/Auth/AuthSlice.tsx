86c795ac2f355cb28af1a910a5ce587874943dea
KieuTruomng <kieutruong8201@gmail.com>
2025-02-25 14:50:04 +0700

    Create homeScreen

--- src/Reduxs/Auth/AuthSlice.tsx
+++ src/Reduxs/Auth/AuthSlice.tsx
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AccountInfor, DataLoginType } from "../../AxiosConfig/DataType";
 
 const initialState: AccountInfor = {
-  isAuth: false,
+  isAuth: true,
   info: null,
   departmentid: "",
   departmentName: "",
